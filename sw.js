3d28cd5d26cfbb763e4b97300e9df2a72fcec6b6
coolcryptomaniac <33383333+coolcryptomaniac@users.noreply.github.com>
2026-08-23 02:10:39 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -16,7 +16,7 @@
  *    caching it would blow the origin's storage quota for no benefit.
  * ------------------------------------------------------------------------- */
 
-var VERSION = 'rw-v105-pipeline'
+var VERSION = 'rw-v106-investor-fix'
 var HTML_CACHE = VERSION + '-html';
 var ASSET_CACHE = VERSION + '-assets';
 
